6001c7f1056f496ddfcf0d81af70731c1c099561
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2021-10-08 21:18:03 -0700

    added two sockets

--- index.js
+++ index.js
@@ -8,10 +8,21 @@ const io = new Server(server);
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-
+//Notifies socket when a user connects
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  //Notifies socket when a user disconnects
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    io.emit('user disconnected');
+    });
+    //shows message to the chat
+    socket.on('chat message', (msg) => {
+        socket.broadcast.emit('chat message', msg);
+    });
 });
+
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
